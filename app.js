5c281ae8fe4465d55d6af948879cd93732245791
zohidbe <zohaxaydarov@gmail.com>
2024-08-13 16:54:42 +0500

    homwork end

---
+++ app.js
@@ -0,0 +1,29 @@
+// let filteredItems = arr.filter(item => item.price && item.gift);
+// console.log(filteredItems);
+
+// task 2
+
+// let tottalPrice = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//     tottalPrice += arr[i].price
+// }
+// console.log(tottalPrice);
+
+// task 3
+// let result = arr.filter((item) => {
+//     if (!item.gift)
+//         item.gift = 'dinay 1.5 litr senga padarka';
+//     return item;
+// })
+// console.log(result);
+
+
+let arr = [
+{ name: "product1", price: 400, id: 1, age:23, discount: 20},
+{ name: "product2", price: 450, id: 2, age:19, discount: 15},
+{ name: "product3", price: 360, id: 3, age:34, discount: 5,   gift: "redmi 13 pro"},
+{ name: "product4", price: 220, id: 4, age:32, discount: 10},
+{ name: "product5", price: 120, id: 5, age:12, discount: 20,  gift: "iphone 15 pro"}
+]
+
